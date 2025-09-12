8c989b29e32cffd814a9d38f945638d187918165
FlazeFy <79736302+FlazeFy@users.noreply.github.com>
2025-09-12 19:48:07 +0700

    [Mobile] Init Swift

---
+++ wardrobe/wardrobe/wardrobeApp.swift
@@ -0,0 +1,17 @@
+//
+//  wardrobeApp.swift
+//  wardrobe
+//
+//  Created by Leonardho R Sitanggang on 12/09/25.
+//
+
+import SwiftUI
+
+@main
+struct wardrobeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
